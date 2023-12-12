b9b745bd764d40d52a5a38920fad89f05f64b76a
wayne <QWE24367705@gmail.com>
2023-12-12 15:05:32 +0800

    首頁建置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,31 @@
+'use client'
+import { useRef,useEffect } from 'react'
+import { Application } from '@splinetool/runtime'
+import { useSkillToggle,usePortfolioToggle } from '@/store/ToggleStore'
 
 export default function Home() {
+  const {skill, setSkillToggle} = useSkillToggle()
+  const {portfolio, setPortfolioToggle} = usePortfolioToggle()
+  const canvas = useRef<HTMLCanvasElement>(null)
+  
+  useEffect(()=>{
+    const app = new Application(canvas.current!);
+    app.load(process.env.NEXT_PUBLIC_INDEX!)
+    .then(()=>{
+      app.addEventListener('mouseDown',(e)=>{
+        if (e.target.name === 'skill'){
+          setSkillToggle()
+        }
+        if(e.target.name === 'otherbox'){
+          setPortfolioToggle()
+        }
+      })
+    })
+  },[])
+
   return (
-    <main className="">personal website</main>
+    <>
+      <canvas ref={canvas} className='absolute z-0 w-[100vw]'/>
+    </>
   )
 }
